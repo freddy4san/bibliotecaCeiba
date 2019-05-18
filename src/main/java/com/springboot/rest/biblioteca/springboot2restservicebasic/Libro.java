74ac51b0778d1e5e8a2982d649b08f3b5a5c1c34
Kevin Enrique Arciniegas Chacon <kear241007@gmail.com>
2019-05-18 15:57:58 -0500

    entity de h2

--- src/main/java/com/springboot/rest/biblioteca/springboot2restservicebasic/Libro.java
+++ src/main/java/com/springboot/rest/biblioteca/springboot2restservicebasic/Libro.java
@@ -3,43 +3,85 @@ package com.springboot.rest.biblioteca.springboot2restservicebasic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Date;
 
-@Entity (name="libro")
+@Entity(name = "libro")
 public class Libro {
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String ISBN;
-	private boolean prestado;
-	
-	
-	
-	public Libro(Long id,String ISBN,boolean prestado) {
-		super();
-		this.id =id;
-		this.ISBN = ISBN;
-		this.prestado = prestado;
-	}
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getISBN() {
-		return ISBN;
-	}
-	public void setISBN(String iSBN) {
-		ISBN = iSBN;
-	}
-	public boolean isPrestado() {
-		return prestado;
-	}
-	public void setPrestado(boolean prestado) {
-		this.prestado = prestado;
-	}
-	
-	
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String ISBN;
+    private boolean prestado;
+    private String nombre;
+    private Long ejemplar;
+    private Date fechaCreada;
+    private Date fechaActualizada;
+
+
+    public Libro(Long id, String ISBN, boolean prestado, String nombre, Long ejemplar, Date fechaCreada, Date fechaActualizada) {
+        super();
+        this.id = id;
+        this.ISBN = ISBN;
+        this.prestado = prestado;
+        this.nombre = nombre;
+        this.ejemplar = ejemplar;
+        this.fechaCreada = fechaCreada;
+        this.fechaActualizada = fechaActualizada;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public boolean isPrestado() {
+        return prestado;
+    }
+
+    public void setPrestado(boolean prestado) {
+        this.prestado = prestado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getEjemplar() {
+        return ejemplar;
+    }
+
+    public void setEjemplar(Long ejemplar) {
+        this.ejemplar = ejemplar;
+    }
+
+    public Date getFechaCreada() {
+        return fechaCreada;
+    }
+
+    public void setFechaCreada(Date fechaCreada) {
+        this.fechaCreada = fechaCreada;
+    }
+
+    public Date getFechaActualizada() {
+        return fechaActualizada;
+    }
+
+    public void setFechaActualizada(Date fechaActualizada) {
+        this.fechaActualizada = fechaActualizada;
+    }
 }
