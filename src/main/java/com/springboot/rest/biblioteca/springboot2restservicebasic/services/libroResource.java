bd55294a811a39f7c87868d3f524e95ab32cd4ee
freddy Sandoval <falexandersandoval@gmail.com>
2019-05-18 17:20:16 -0500

    modificacion libroResource

--- src/main/java/com/springboot/rest/biblioteca/springboot2restservicebasic/services/libroResource.java
+++ src/main/java/com/springboot/rest/biblioteca/springboot2restservicebasic/services/libroResource.java
@@ -1,23 +1,24 @@
 package com.springboot.rest.biblioteca.springboot2restservicebasic.services;
-import com.springboot.rest.biblioteca.springboot2restservicebasic.entity.LibroPrueba;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.rest.biblioteca.springboot2restservicebasic.Libro;
+
 @RestController
 public class libroResource {
 	
 	
 	@GetMapping("/libros")
-	public List<LibroPrueba> retrieveAllStudents() {
+	public List<Libro> retrieveAllStudents() {
 		//return studentRepository.findAll();
-		LibroPrueba libro1 = new LibroPrueba((long) 1, "123456", false);
-		ArrayList<LibroPrueba> datos = new ArrayList<LibroPrueba>();
+		Libro libro1 = new Libro((long) 1, "123456", false);
+		ArrayList<Libro> datos = new ArrayList<Libro>();
 		
 		datos.add(libro1);
+		
 		
 		return datos;
 	}
