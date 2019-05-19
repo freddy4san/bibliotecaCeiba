fe63ab5d9584d3891bf7129ccdd1ffd82c201f4c
freddy Sandoval <falexandersandoval@gmail.com>
2019-05-19 09:28:45 -0500

    agregados datos iniciales

--- src/main/java/com/springboot/rest/biblioteca/springboot2restservicebasic/services/libroResource.java
+++ src/main/java/com/springboot/rest/biblioteca/springboot2restservicebasic/services/libroResource.java
@@ -1,12 +1,15 @@
 package com.springboot.rest.biblioteca.springboot2restservicebasic.services;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.rest.biblioteca.springboot2restservicebasic.entity.Libro;
+import com.springboot.rest.biblioteca.springboot2restservicebasic.exception.LibroNotFoundException;
 import com.springboot.rest.biblioteca.springboot2restservicebasic.repository.LibroRepository;
 
 @RestController
@@ -19,6 +22,16 @@ public class LibroResource {
 	@GetMapping("/libros")
 	public List<Libro> retrieveAllStudents() {
 		return libroRepository.findAll();
+	}
+	
+	@GetMapping("/libros/{id}")
+	public Libro retrieveStudent(@PathVariable long id) {
+		Optional<Libro> student = libroRepository.findById(id);
+
+		if (!student.isPresent())
+			throw new LibroNotFoundException("id-" + id);
+
+		return student.get();
 	}
 	
 	@GetMapping("/prueba")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into LIBRO 
+values(10001, 'A1234568',true);
+
+insert into LIBRO 
+values(10002, 'E1234568',false);
